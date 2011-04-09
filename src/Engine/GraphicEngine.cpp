d5cc58fb18fc820fe2105e4db0ef92df8a210bf3
Bastien SIGNORET <bastien@signoret.eu>
2011-04-09 23:54:58 +0200

    Implémentation de GraphicEngine

--- src/Engine/GraphicEngine.cpp
+++ src/Engine/GraphicEngine.cpp
@@ -1,38 +1,83 @@
+#include <SDL/SDL.h>
+#include <GL/gl.h>
+#include <GL/glu.h>
+#include <string.h>
+
 #include "GraphicEngine.h"
 #include "Scene.h"
 #include "Camera.h"
 #include "Object.h"
 
-using namespace Engine;
-
-GraphicEngine::GraphicEngine()
+namespace Engine
 {
-    //ctor
+
+GraphicEngine::GraphicEngine(string name,unsigned int w, unsigned int h,unsigned int nbCamera,unsigned int nbScene)
+{
+    SDL_Init(SDL_INIT_VIDEO);
+
+    SDL_WM_SetCaption(name.c_str(),NULL);
+    SDL_SetVideoMode(w, h, 32, SDL_OPENGL);
+
+    camera = new vector<Camera*>(nbCamera);
+    scene = new vector<Scene*>(nbScene);
+
+    /**
+    Ajout d'une scene par defaut
+    **/
+    scene->push_back(new Scene(TS_Static));
 }
 
 GraphicEngine::~GraphicEngine()
 {
-    //dtor
+    //TODO libérer chaque scene et caméra
+    delete camera;
+    delete scene;
+    SDL_Quit();
 }
 
-void GraphicEngine::addCamera(Camera camera)
+unsigned int GraphicEngine::addCamera(Camera *camera)
 {
+    this->camera->push_back(camera);
 
+    //Retourne l'id de la nouvelle camera
+    return this->camera->size()-1;
 }
-unsigned int GraphicEngine::addScene(Scene scene)
+
+unsigned int GraphicEngine::addScene(Scene *scene)
 {
+    this->scene->push_back(scene);
 
+    //Retourne l'id de la nouvelle scene
+    return this->scene->size()-1;
 }
 
-void GraphicEngine::addSceneObject(Object object,unsigned idScene)
+void GraphicEngine::addSceneObject(Object *object,unsigned idScene)
 {
-
+    (*scene)[idScene]->addObject(object);
 }
-void GraphicEngine::addSceneObject(Object object,Scene scene)
+
+void GraphicEngine::addSceneObject(Object *object,Scene *scene)
 {
-
+    scene->addObject(object);
 }
-void GraphicEngine::update()
+
+void GraphicEngine::update(Camera *camera)
 {
+    //Mise à jours de la caméra
+    camera->update();
 
+    //Mise à jours des scenes
+    vector<Scene*>::iterator it;
+
+    for ( it=scene->begin() ; it < scene->end(); it++ )
+    {
+        (*it)->update();
+    }
 }
+
+void GraphicEngine::update(unsigned int idCamera)
+{
+    this->update((*camera)[idCamera]);
+}
+
+}//namespace Engine
--- src/Engine/GraphicEngine.h
+++ src/Engine/GraphicEngine.h
@@ -2,6 +2,7 @@
 #define GRAPHICENGINE_H
 
 #include <vector>
+#include <string>
 #include "Scene.h"
 #include "Camera.h"
 
@@ -15,40 +16,40 @@ namespace Engine
     class GraphicEngine
     {
         public:
-            GraphicEngine();
+            GraphicEngine(string name,unsigned int w, unsigned int h,unsigned int nbCamera=1,unsigned int nbScene=1);
             virtual ~GraphicEngine();
 
             /**
             Ajoute une caméra à gérer
             **/
-            void addCamera(Camera camera);
+            unsigned int addCamera(Camera *camera);
 
             /**
             Ajoute une scene à gérer
             **/
-            unsigned int addScene(Scene scene);
+            unsigned int addScene(Scene *scene);
 
             /**
             Ajoute un objet à une scene
             **/
-            void addSceneObject(Object object,unsigned idScene=0);
-            void addSceneObject(Object object,Scene scene);
+            void addSceneObject(Object *object,unsigned idScene=0);
+            void addSceneObject(Object *object,Scene *scene);
 
             /**
             Met à jour l'affichage
             **/
-            void update();
+            void update(Camera *camera);
+            void update(unsigned int idCamera=0);
         private:
+            /**
+            Liste des caméras à gérer
+            **/
+            vector<Camera*>* camera;
 
             /**
             Liste des caméras à gérer
             **/
-            vector<Camera> camera;
-
-            /**
-            Liste des caméras à gérer
-            **/
-            vector<Scene> scene;
+            vector<Scene*>* scene;
     };
 }
 #endif // GRAPHICENGINE_H
