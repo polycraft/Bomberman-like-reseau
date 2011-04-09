faa63909d2fd83bd14fb4609b2faeecdd86aac6c
Bastien SIGNORET <bastien@signoret.eu>
2011-04-10 00:26:14 +0200

    Ajout de la classe OpenGL

---
+++ src/Engine/OpenGL2.cpp
@@ -0,0 +1,11 @@
+#include "OpenGL2.h"
+
+OpenGL2::OpenGL2()
+{
+    //ctor
+}
+
+OpenGL2::~OpenGL2()
+{
+    //dtor
+}
---
+++ src/Engine/OpenGL2.h
@@ -0,0 +1,12 @@
+#ifndef OPENGL2_H
+#define OPENGL2_H
+
+
+class OpenGL2
+{
+    public:
+        OpenGL2();
+        virtual ~OpenGL2();
+};
+
+#endif // OPENGL2_H
