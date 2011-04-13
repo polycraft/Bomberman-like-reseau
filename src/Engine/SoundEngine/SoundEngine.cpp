a9ba6964c565b82d8db9464ca13622283aada667
Bastien SIGNORET <bastien@signoret.eu>
2011-04-13 02:29:29 +0200

    Implémentation du moteur de son avec fmod

---
+++ src/Engine/SoundEngine/SoundEngine.cpp
@@ -0,0 +1,102 @@
+#include "SoundEngine.h"
+#include <fmodex/fmod.h>
+
+namespace Engine
+{
+SoundEngine::SoundEngine(unsigned int nbChannel)
+{
+    //Initialise fmod
+    FMOD_System_Create(&system);
+	FMOD_System_Init(system, nbChannel, FMOD_INIT_NORMAL, NULL);
+}
+
+SoundEngine::~SoundEngine()
+{
+    //libere fmod
+    FMOD_System_Close(system);
+	FMOD_System_Release(system);
+}
+
+SoundEngine *SoundEngine::getInstance()
+{
+    if(instance==NULL)
+    {
+        instance=new SoundEngine(32);
+    }
+    return instance;
+}
+
+FMOD_SYSTEM *SoundEngine::getSystem()
+{
+    return system;
+}
+
+void SoundEngine::playSound(Sound &sound)
+{
+    FMOD_System_PlaySound(system, FMOD_CHANNEL_FREE, sound.getSound(), 0, NULL);
+}
+
+void SoundEngine::playSound(FMOD_CHANNELINDEX channel,Sound &sound)
+{
+    FMOD_System_PlaySound(system, channel, sound.getSound(), 0, NULL);
+}
+
+FMOD_CHANNEL *SoundEngine::getChannel(unsigned int idChannel)
+{
+    FMOD_CHANNEL *channel;
+    FMOD_System_GetChannel(system, idChannel, &channel);
+    return channel;
+}
+
+void SoundEngine::togglePauseChannel(unsigned int channel)
+{
+    FMOD_BOOL etat;
+    FMOD_CHANNEL *canal=getChannel(channel);
+    FMOD_Channel_GetPaused(canal, &etat);
+
+    if (etat == 1) // Si la chanson est en pause
+        FMOD_Channel_SetPaused(canal, 0); // On enlève la pause
+    else // Sinon, elle est en cours de lecture
+        FMOD_Channel_SetPaused(canal, 1); // On met en pause
+}
+
+void SoundEngine::setVolumeChannel(unsigned int channel,float volume)
+{
+    FMOD_Channel_SetVolume(getChannel(channel),volume);
+}
+
+void SoundEngine::stopChannel(unsigned int channel)
+{
+    FMOD_Channel_Stop(getChannel(channel));
+}
+
+FMOD_CHANNELGROUP *SoundEngine::getChannelGroup()
+{
+    FMOD_CHANNELGROUP *channel;
+    FMOD_System_GetMasterChannelGroup(system, &channel);
+    return channel;
+}
+
+void SoundEngine::togglePauseAllChannel()
+{
+    FMOD_BOOL etat;
+    FMOD_CHANNELGROUP *channel=getChannelGroup();
+    FMOD_ChannelGroup_GetPaused(channel, &etat);
+
+    if (etat == 1) // Si la chanson est en pause
+        FMOD_ChannelGroup_SetPaused(channel, 0); // On enlève la pause
+    else // Sinon, elle est en cours de lecture
+        FMOD_ChannelGroup_SetPaused(channel, 1); // On met en pause
+}
+
+void SoundEngine::setVolumeAllChannel(float volume)
+{
+    FMOD_ChannelGroup_SetVolume(getChannelGroup(),volume);
+}
+
+void SoundEngine::stopAllChannel()
+{
+    FMOD_ChannelGroup_Stop(getChannelGroup());
+}
+
+}
---
+++ src/Engine/SoundEngine/SoundEngine.h
@@ -0,0 +1,90 @@
+#ifndef SOUNDENGINE_H
+#define SOUNDENGINE_H
+
+#include <fmodex/fmod.h>
+#include <stdlib.h>
+
+#include "../Sound.h"
+
+namespace Engine
+{
+/**
+Moteur de gestion de son à base de FMOD fx
+**/
+class SoundEngine
+{
+    public:
+
+        virtual ~SoundEngine();
+
+        /**
+        Retourne l'instance du singleton
+        **/
+        static SoundEngine *getInstance();
+
+        /**
+        Retourne systeme
+        **/
+        FMOD_SYSTEM *getSystem();
+
+        /**
+        Joue un son
+        **/
+        void playSound(Sound &sound);
+
+        /**
+        Joue un son sur un chanel
+        **/
+        void playSound(FMOD_CHANNELINDEX channel,Sound &sound);
+
+        /**
+        Met en pause ou reprend la lecture d'un canal
+        **/
+        void togglePauseChannel(unsigned int channel);
+
+        /**
+        Régle le volume d'un canal
+        **/
+        void setVolumeChannel(unsigned int channel,float volume);
+
+        /**
+        Arrete la lecture sur un canal
+        **/
+        void stopChannel(unsigned int channel);
+
+         /**
+        Met en pause ou reprend la lecture de tous les canaux
+        **/
+        void togglePauseAllChannel();
+
+        /**
+        Régle le volume de tous les canaux
+        **/
+        void setVolumeAllChannel(float volume);
+
+        /**
+        Arrete la lecture de tous les canaux
+        **/
+        void stopAllChannel();
+
+    protected:
+    private:
+        static SoundEngine *instance;
+        FMOD_SYSTEM *system;
+
+        SoundEngine(unsigned int nbChannel=32);
+
+        /**
+        Retourne un canal à partir de son id
+        **/
+        FMOD_CHANNEL *getChannel(unsigned int idChannel);
+
+        /**
+        Retourne le canal maitre
+        **/
+        FMOD_CHANNELGROUP *getChannelGroup();
+};
+//Initialisation de l'instance
+SoundEngine *SoundEngine::instance = NULL;
+}
+#endif // SOUNDENGINE_H
