ef3efdb7c476777eff369f1a1e7990c0356cdf03
Bastien SIGNORET <bastien@signoret.eu>
2011-04-13 02:30:08 +0200

    Création de la ressource Sound

---
+++ src/Engine/Sound.cpp
@@ -0,0 +1,20 @@
+#include "Sound.h"
+#include <fmodex/fmod.h>
+
+namespace Engine
+{
+Sound::Sound()
+{
+    //ctor
+}
+
+Sound::~Sound()
+{
+    //dtor
+}
+
+FMOD_SOUND *Sound::getSound()
+{
+
+}
+}
---
+++ src/Engine/Sound.h
@@ -0,0 +1,21 @@
+#ifndef SOUND_H
+#define SOUND_H
+
+#include "Ressource.h"
+#include <fmodex/fmod.h>
+
+namespace Engine
+{
+    class Sound : public Ressource
+    {
+        public:
+            Sound();
+            virtual ~Sound();
+
+            FMOD_SOUND *getSound();
+
+        protected:
+        private:
+    };
+}
+#endif // SOUND_H
