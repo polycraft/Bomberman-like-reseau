102786f148c0685505fe4056c59e1607f4663c67
Bastien SIGNORET <bastien@signoret.eu>
2011-05-14 00:12:47 +0200

    implémentation de la gestion de collision

---
+++ src/CollisionDetector.cpp
@@ -0,0 +1,48 @@
+#include "CollisionDetector.h"
+
+CollisionDetector::CollisionDetector(Map* map):map(map)
+{
+
+}
+
+virtual CollisionDetector::~CollisionDetector()
+{
+
+}
+
+ECollision CollisionDetector::detect(EType type,int x,int y)
+{
+    if(type==T_Bomberman)
+    {
+        switch(map[x][y]->getType())
+        {
+            case T_Bomb:
+            case T_BreakableBloc:
+            case T_StaticBloc:
+                return C_Block;
+            case T_Explosion:
+                return C_Kill;
+            case T_Bonus:
+                return C_Bonus;
+            default:
+                return C_Nothing;
+        }
+    }
+    else if(type==T_Explosion)
+    {
+        switch(map[x][y]->getType())
+        {
+            case T_Bomb:
+                return C_Explose;
+            case T_BreakableBloc:
+            case T_StaticBloc:
+            case T_Explosion:
+                return C_Block;
+            case T_Bonus:
+                return C_Kill;
+            default:
+                return C_Nothing;
+        }
+    }
+    return C_Nothing;
+}
