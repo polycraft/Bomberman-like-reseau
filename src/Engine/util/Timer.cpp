00192cfec3304f3a48b9ded01594f6c66599aab5
Bastien SIGNORET <bastien@signoret.eu>
2011-05-13 22:05:49 +0200

    implémentation du timer

---
+++ src/Engine/util/Timer.cpp
@@ -0,0 +1,78 @@
+#include "Timer.h"
+#include <SDL/SDL.h>
+
+namespace Engine
+{
+    Timer* Timer::instance=NULL;
+
+Timer *Timer::getTimer()
+{
+    if(instance==NULL)
+    {
+        instance=new Timer();
+    }
+    return instance;
+}
+
+Timer::Timer()
+{
+
+}
+
+Timer::~Timer()
+{
+
+}
+
+struct SObserverTimer* Timer::createSObserverTime(IObserverTimer* observer,int delay)
+{
+    struct SObserverTimer* sObserver=new struct SObserverTimer;
+    sObserver->start=SDL_GetTicks();
+    sObserver->observer=observer;
+    sObserver->delay=delay;
+
+    return sObserver;
+}
+
+void Timer::addListener(IObserverTimer* observer,int delay)
+{
+    struct SObserverTimer* sObserver=createSObserverTime(observer,delay);
+
+    listener.push_back(sObserver);
+}
+
+void Timer::addListenerOnce(IObserverTimer* observer,int delay)
+{
+    struct SObserverTimer* sObserver=createSObserverTime(observer,delay);
+
+    listenerOnce.push_back(sObserver);
+}
+
+void Timer::update()
+{
+    //On verifie les listeners
+    vector< struct SObserverTimer* >::iterator it=listener.begin();
+    struct SObserverTimer* sObserver=NULL;
+
+    while(it<listener.end())
+    {
+        sObserver=*it;
+        if(sObserver->start+sObserver->delay >= SDL_GetTicks())
+        {
+            sObserver->observer->updateTimer();
+        }
+    }
+
+    //On vérifie les listeners qui doivent etre appellé qu'une seul fois
+    it=listenerOnce.begin();
+    while(it<listenerOnce.end())
+    {
+        sObserver=*it;
+        if(sObserver->start+sObserver->delay >= SDL_GetTicks())
+        {
+            sObserver->observer->updateTimer();
+            it=listenerOnce.erase(it);
+        }
+    }
+}
+}
---
+++ src/Engine/util/Timer.h
@@ -0,0 +1,35 @@
+#ifndef TIMER_H
+#define TIMER_H
+
+#include <vector>
+#include "IObserverTimer.h"
+using namespace std;
+namespace Engine
+{
+
+struct SObserverTimer
+{
+    IObserverTimer* observer;
+    unsigned int delay;
+    unsigned int start;
+};
+
+class Timer
+{
+    public:
+        Timer* getTimer();
+        void addListener(IObserverTimer*,int delay);
+        void addListenerOnce(IObserverTimer*,int delay);
+        void update();
+        virtual ~Timer();
+    protected:
+    private:
+        Timer();
+        struct SObserverTimer* createSObserverTime(IObserverTimer* observer,int delay);
+        static Timer* instance;
+        vector<struct SObserverTimer*> listener;
+        vector<struct SObserverTimer*> listenerOnce;
+};
+}
+
+#endif // TIMER_H
