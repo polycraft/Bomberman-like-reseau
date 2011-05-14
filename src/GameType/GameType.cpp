da2a48b236e882339044a3d3b63a683b20dbc1d3
FLoCaz <florian.cazalet@fre.fr>
2011-05-14 13:16:38 +0200

    Ajout des Classe de Gametype CTF

--- src/GameType/GameType.cpp
+++ src/GameType/GameType.cpp
@@ -7,7 +7,7 @@ GameType::GameType()
 
 GameType::~GameType()
 {
-<<<<<<< HEAD
+
 }
 
 void GameType::explode(Bomb* bomb,int speed,int power)
@@ -15,7 +15,4 @@ void GameType::explode(Bomb* bomb,int speed,int power)
 
 }
 
-=======
 
-}
->>>>>>> b16bb09b97a161735047f662f05780757e96306d
