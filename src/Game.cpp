7cf050140b2f86e2f90edcd226157ead8d15562d
Bastien SIGNORET <bastien@signoret.eu>
2011-05-15 22:53:36 +0200

    Création de la classe Game

---
+++ src/Game.cpp
@@ -0,0 +1,21 @@
+#include "Game.h"
+
+Game::Game()
+{
+    //ctor
+}
+
+Game::~Game()
+{
+    //dtor
+}
+
+void Game::updateTimer()
+{
+
+}
+
+Map *Game::getMap()
+{
+
+}
---
+++ src/Game.h
@@ -0,0 +1,20 @@
+#ifndef GAME_H
+#define GAME_H
+#include "Engine/util/IObserverTimer.h"
+#include "Map.h"
+
+class Game : public Engine::IObserverTimer
+{
+    public:
+        Game();
+        virtual ~Game();
+
+        void run();
+        void updateTimer();
+
+        Map *getMap();
+    protected:
+    private:
+};
+
+#endif // GAME_H
