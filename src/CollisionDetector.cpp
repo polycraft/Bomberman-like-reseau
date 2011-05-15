180cc4fadb05197d6195ce6656ad936ff09e34fd
Bastien SIGNORET <bastien@signoret.eu>
2011-05-15 17:14:01 +0200

    Ajout d'une vérification de pointeur NULL

--- src/CollisionDetector.cpp
+++ src/CollisionDetector.cpp
@@ -12,36 +12,40 @@ CollisionDetector::~CollisionDetector()
 
 ECollision CollisionDetector::detect(EType type,int x,int y)
 {
-    if(type==T_Bomberman)
+    Type *object=map->get(x,y);
+    if(object!=NULL)
     {
-        switch(map->get(x,y)->getType())
+        if(type==T_Bomberman)
         {
-            case T_Bomb:
-            case T_BreakableBloc:
-            case T_StaticBloc:
-                return C_Block;
-            case T_Explosion:
-                return C_Kill;
-            case T_Bonus:
-                return C_Bonus;
-            default:
-                return C_Nothing;
+            switch(object->getType())
+            {
+                case T_Bomb:
+                case T_BreakableBloc:
+                case T_StaticBloc:
+                    return C_Block;
+                case T_Explosion:
+                    return C_Kill;
+                case T_Bonus:
+                    return C_Bonus;
+                default:
+                    return C_Nothing;
+            }
         }
-    }
-    else if(type==T_Explosion)
-    {
-        switch(map->get(x,y)->getType())
+        else if(type==T_Explosion)
         {
-            case T_Bomb:
-                return C_Explose;
-            case T_BreakableBloc:
-            case T_StaticBloc:
-            case T_Explosion:
-                return C_Block;
-            case T_Bonus:
-                return C_Kill;
-            default:
-                return C_Nothing;
+            switch(object->getType())
+            {
+                case T_Bomb:
+                    return C_Explose;
+                case T_BreakableBloc:
+                case T_StaticBloc:
+                case T_Explosion:
+                    return C_Block;
+                case T_Bonus:
+                    return C_Kill;
+                default:
+                    return C_Nothing;
+            }
         }
     }
     return C_Nothing;
