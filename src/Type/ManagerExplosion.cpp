289dc969403884f205c672b1bb5b5624f1064aee
FLoCaz <florian.cazalet@fre.fr>
2011-05-17 02:15:32 +0200

    Maj du ManagerExplosion

--- src/Type/ManagerExplosion.cpp
+++ src/Type/ManagerExplosion.cpp
@@ -1,12 +1,17 @@
 #include "ManagerExplosion.h"
 
 
-ManagerExplosion::ManagerExplosion(int id, int speed, int power, EExplose typeExplose, Map *map)
+ManagerExplosion::ManagerExplosion(int x, int y, int id, int speed, int power, EExplose typeExplose, GameType *gametype)
 {
 	this->id = id;
 	this->speed = speed;
 	this->power = power;
 	this->typeExplosion = typeExplose;
+	this->isEnd = false;
+	this->gameType = gametype;
+
+	Timer::getTimer()->addListener(this,500/this->speed);
+
 	if(this->typeExplosion == T_Emitter)
 	{
 		this->listeExplosions.push_back(new Explosion(T_Emitter));
@@ -16,10 +21,17 @@ ManagerExplosion::ManagerExplosion(int id, int speed, int power, EExplose typeEx
 		this->listeExplosions.push_back(new Explosion(T_End));
 	}
 
+
 }
 
 ManagerExplosion::~ManagerExplosion()
 {
+	vector<Explosion*>::iterator it=this->listeExplosions.begin();
+	while(it < this->listeExplosions.end())
+	{
+		(*it)->destroy();
+		it++;
+	}
 }
 
 void ManagerExplosion::nextExplose()
@@ -33,12 +45,31 @@ void ManagerExplosion::nextExplose()
 
 void ManagerExplosion::endExplose()
 {
-	vector<Explosion*>::iterator it=this->listeExplosions.begin();
-	while(it < this->listeExplosions.end())
+	Timer::getTimer()->removeListener(this,500/this->speed);
+	Timer::getTimer()->addListener(this,1000);
+}
+
+EExplose ManagerExplosion::getType()
+{
+	return this->typeExplosion;
+}
+
+void ManagerExplosion::updateTimer(unsigned int delay)
+{
+	if(!this->isEnd)
 	{
-		(*it)->destroy();
-		it++;
+		this->gameType->updateExplosion(this,this->power,this->x,this->y);
+	}
+	else
+	{
+		Timer::getTimer()->removeListener(this,1000);
+		this->gameType->destroyManagerExplose(this);
+
 	}
 
+}
 
+int ManagerExplosion::getID()
+{
+	return this->id;
 }
