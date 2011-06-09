f9afb0ebf2e0be88559e8164f0206c2b1e920c12
FLoCaz <florian.cazalet@fre.fr>
2011-06-09 07:18:28 +0200

    MAJ Client

--- src/GameType/Classic/HurryUp.cpp
+++ src/GameType/Classic/HurryUp.cpp
@@ -44,43 +44,16 @@ namespace GameTypeSpace
 		{
 			if(delay == this->actuTime)
 			{
-				/*if(blocz>=0)
+				if(blocz>=0)
 					{
 						this->bloc->setCoordonnes(this->blocx*10+15,this->blocy*10+15,this->blocz+5);
 						this->blocz -= Engine::Timer::getTimer()->getTimePerFrame()/4;
 					}
 				else
 				{
-					this->blocDown = true;
+					//this->bloc->destroy();
 					Engine::Timer::getTimer()->removeListener(this,this->actuTime);
-						EType type;
-						if(this->gameType->getGame()->getMap()->get(this->blocx,this->blocy) != NULL)
-						{
-							type = this->gameType->getGame()->getMap()->get(this->blocx,this->blocy)->getType();
-							if(type == T_BreakableBloc || type == T_Bonus)
-							{
-								this->gameType->getGame()->getMap()->addObject(new StaticBloc(), this->blocx, this->blocy, T_Dyn);
-
-							}
-							else if( type == T_Bomb)
-							{
-								this->gameType->getGame()->getMap()->addObject(new StaticBloc(), this->blocx, this->blocy, T_Dyn);
-								this->gameType->getPlayer()->setProperty<int>(PB_nbBomb, this->gameType->getPlayer()->getProperty<int>(PB_nbBomb)+1);
-							}
-							else if(type == T_Explosion)
-							{
-								Explosion* explosion = dynamic_cast<Explosion*>(this->gameType->getGame()->getMap()->get(this->blocx,this->blocy));
-								explosion->getExplosionFlare()->removeExplosion(explosion);
-								this->gameType->getGame()->getMap()->addObject(new StaticBloc(), this->blocx, this->blocy, T_Dyn);
-
-							}
-						}
-						else
-						{
-							this->gameType->getGame()->getMap()->addObject(new StaticBloc(), this->blocx, this->blocy, T_Dyn);
-		
-						}
-				}*/
+				}
 			}
 		}
 
@@ -92,7 +65,7 @@ namespace GameTypeSpace
 				case 'h'://HurryBloc
 				{
 					
-					/*if(!this->blocDown)
+					if(!this->blocDown)
 					{
 						
 						EType type;
@@ -126,14 +99,14 @@ namespace GameTypeSpace
 							//Engine::Timer::getTimer()->removeListener(this,this->actuTime);
 		
 						}
-					}*/
+					}
 
-					//Engine::Timer::getTimer()->addListener(this,this->actuTime);
-					/*PaquetHurry *paquetHurry=paquet.getData<PaquetHurry*>();
+					Engine::Timer::getTimer()->addListener(this,this->actuTime);
+					PaquetHurry *paquetHurry=paquet.getData<PaquetHurry*>();
 					this->blocx = paquetHurry->x;
 					this->blocy = paquetHurry->y;
 					this->blocz = 50;
-					this->blocDown = false;*/
+					this->blocDown = false;
 				}
 				break;
 			}
